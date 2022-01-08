cdf190360f821aced149116075df42584ab11fcf
pradeep-selva <pradeepselvaraju@gmail.com>
2022-01-08 19:27:44 +0530

    ft: setup SwapRouter.t.sol

---
+++ src/test/SwapRouter.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../SwapRouter.sol";
+import "../interface/IOneSplit.sol";
+import "./utils/IWETH9.sol";
+
+import "../../lib/ds-test/src/test.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract ContractTest is DSTest {
+    SwapRouter private swapRouter;
+
+    IOneSplit private constant ONE_SPLIT_AUDIT =
+        IOneSplit(0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E);
+    IERC20 private constant USDC =
+        IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IWETH9 private constant WETH =
+        IWETH9(payable(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)));
+
+    function setUp() public {
+        swapRouter = new SwapRouter(ONE_SPLIT_AUDIT, USDC);
+        emit log_named_address("Self", self());
+
+        getWETHFromETH(10);
+    }
+
+    function testSuccess() public pure {
+        assert(true);
+    }
+
+    function getWETHFromETH(uint256 _ethToSwap) internal {
+        WETH.deposit{value: _ethToSwap * 10**18}();
+
+        emit log_named_uint("ETH Balance", self().balance / 10**18);
+        emit log_named_uint(
+            "WETH Balance",
+            WETH.balanceOf(self()) / 10**WETH.decimals()
+        );
+    }
+
+    function self() internal view returns (address) {
+        return address(this);
+    }
+}
