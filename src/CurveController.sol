14d4c774ed7c5897008ab8aa166cd6345fe0260f
pradeep-selva <pradeepselvaraju@gmail.com>
2022-01-13 11:43:43 +0530

    ft: openPosition() to stake cvxCRV

--- src/CurveController.sol
+++ src/CurveController.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./interface/ISwapRouter.sol";
+import "./interface/IConvexRewards.sol";
 import "./interface/IPositionHandler.sol";
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -12,16 +13,62 @@ contract CurveController is IPositionHandler {
     using SafeERC20 for IERC20Metadata;
 
     ISwapRouter public swapRouter;
+    // 0x3Fe65692bfCD0e6CF84cB1E7d24108E434A7587e
+    IConvexRewards public baseRewardPool;
 
-    constructor(ISwapRouter _swapRouter) {
+    constructor(ISwapRouter _swapRouter, IConvexRewards _baseRewardPool) {
         swapRouter = _swapRouter;
+        baseRewardPool = _baseRewardPool;
     }
 
     function openPosition(
         uint256 _amount,
         bool _isLong,
         bytes memory _data
-    ) external {}
+    ) external {
+        require(_isLong, "CurveController :: not long");
+        require(
+            _amount > 0 &&
+                _amount <= swapRouter.USDC().balanceOf(address(this)),
+            "CurveController :: amount"
+        );
+
+        if (
+            swapRouter.USDC().allowance(address(this), address(swapRouter)) <
+            type(uint256).max
+        ) {
+            swapRouter.USDC().approve(address(swapRouter), type(uint256).max);
+        }
+        uint256 receivedCRV = swapRouter.estimateAndSwapTokens(
+            true,
+            address(swapRouter.CRV()),
+            _amount,
+            address(this),
+            5,
+            _data
+        );
+
+        if (
+            swapRouter.CRV().allowance(address(this), address(swapRouter)) <
+            type(uint256).max
+        ) {
+            swapRouter.CRV().approve(address(swapRouter), type(uint256).max);
+        }
+        swapRouter.swapOnCRVCVXCRVPool(true, receivedCRV, address(this));
+
+        if (
+            swapRouter.CVXCRV().allowance(
+                address(this),
+                address(baseRewardPool)
+            ) < type(uint256).max
+        ) {
+            swapRouter.CRV().approve(
+                address(baseRewardPool),
+                type(uint256).max
+            );
+        }
+        require(baseRewardPool.stakeAll(), "CurveController :: staking");
+    }
 
     function closePosition(uint256 _amount, bytes memory _data) external {}
 
