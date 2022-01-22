1eb9f07c67ab2c38fe81f8edb587f4026e64b631
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-01-22 12:13:39 +0530

    added request for harvester

---
+++ harvester.md
@@ -0,0 +1,11 @@
+# Feature Request
+Contract to harvest the rewards accumulated from staking `CVXCRV` token. 
+
+## Expected Behaviour
+To convert any new staking rewards to `USDC` token.
+
+## Features
+- [ ] Add/remove swap token.
+- [ ] Swap the tokens using a specific amm than 1inch preferbly.
+- [ ] Support conversion of any new lp tokens back to `USDC` token.
+
---
+++ src/interface/IHarvester.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface IHarvester {
+
+    // Add token to be swapped via 1inch
+    function addSwapToken(address _addr) external;
+
+    // Remove token to be swapped via 1inch
+    function removeSwapToken(address _addr) external;
+
+    // Swap tokens to wantToken 
+    function harvest(address wantToken) external;
+
+    //1inch swap config add any required params
+    function swap(address sourceToken, uint256 slippage) external;
+
+}
